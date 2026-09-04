7076269df39491bf2bfdd28bd109336fe34a1775
Cijid <mmkcijid@gmail.com>
2026-09-04 14:06:38 -0600

    Marker can be sized depending on capability. Added mock data

--- frontend/client/src/components/AreaMap.jsx
+++ frontend/client/src/components/AreaMap.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { MapContainer, TileLayer, Marker, Popup, CircleMarker } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup, Circle } from 'react-leaflet';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 
@@ -12,26 +12,46 @@ function AreaMap() {
 
   // setLatCoord(position[0])
   // setLongCoord(position[1])
+  //Replace with real data later
+  const capabilityStatus = {
+    capability: "UHF",
+    status: "Degraded",
+    latitude: 38,
+    longitude: -104,
+    rangeMiles: 100,
+  };
 
+    const milesToMeters = (miles) => {
+    return miles * 1609.344;
+  };
+
+  const radiusMeters = milesToMeters(capabilityStatus.rangeMiles)
 
   return (
     <div className ="page-container">
       <div className ="header-container">
       </div>
       <div className ="map-container" style={{ height: '500px', width: '100%' }}>
-        <MapContainer center={position} zoom={13} style={{height: '100%', width: '100%'}}>
+        <MapContainer center={position} zoom={5} style={{height: '100%', width: '100%'}}>
           <TileLayer
             attribution="&copy; OpenStreetMap contributors"
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
 
-          <CircleMarker
-          center={position}
-          radius={15}>
+          <Circle
+          center={[
+            capabilityStatus.latitude,
+            capabilityStatus.longitude
+          ]}
+          radius={radiusMeters}>
           <Popup>
-           <p> A pretty CSS3 popup. <br /> Easily Customizable <br /> Position selected: {position[0]}, {position[1]} </p>
+           <p>
+            Capability: {capabilityStatus.capability} <br />
+            Status: {capabilityStatus.status} <br />
+            Impact Radius: {capabilityStatus.rangeMiles} miles <br />
+            Position selected: {capabilityStatus.latitude},{" "}{capabilityStatus.longitude} </p>
           </Popup>
-        </CircleMarker>
+        </Circle>
         </MapContainer>
 
 
