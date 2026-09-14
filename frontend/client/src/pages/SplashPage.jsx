475f2be1fe35439ffd8dfd6f923aaaeea6662302
tonybargas8111-glitch <tonyvargas8111@gmail.com>
2026-09-14 11:47:12 -0700

    Renames view-heading on SplashPage.jsx to view-heading1 on line 30 and view-heading to view-heading2 on line 92. On index.css I changed line 84 to view-heading1 and added color, I changed line 89 to view-heading2 and changed it's color to black.

--- frontend/client/src/pages/SplashPage.jsx
+++ frontend/client/src/pages/SplashPage.jsx
@@ -27,7 +27,7 @@ function SplashPage() {
           </p>
         </div>
 
-        <div className="view-heading">
+        <div className="view-heading1">
           <h2>Select Your Operational View</h2>
           <p>Choose a view based on your mission role.</p>
         </div>
@@ -89,7 +89,7 @@ function SplashPage() {
           </div>
         </div>
         <br></br>
-        <div className="view-heading">
+        <div className="view-heading2">
           <button onClick={() => navigate("/space-coverage")}>
             <h2>Strategic View</h2>
           </button>
